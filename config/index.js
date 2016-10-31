b9abd7af34a67199d5bd87ad233c93a455838699
xyxiao001 <463618622@qq.com>
2016-10-31 22:20:31 +0800

    change port

--- config/index.js
+++ config/index.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8888,
+    port: 8867,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
