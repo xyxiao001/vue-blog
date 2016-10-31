22860327b0746461fe8a754dca2889c21043477b
xyxiao <463618622@qq.com>
2016-10-31 14:43:32 +0800

    one

---
+++ README.md
@@ -0,0 +1,2 @@
+#  a blog for vue  
+##使用vue2.0 vuex sass es6 vue-router2.0
---
+++ src/view/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="content">
+    1
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+  },
+  methods: {
+  },
+  // 页面进入执行
+  mounted () {
+  },
+  // 页面切换
+  destroyed () {
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
