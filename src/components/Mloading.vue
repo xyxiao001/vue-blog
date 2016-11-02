253ffb0f55821405eb4cb1532bdf496e69e71fca
xyxiao001 <463618622@qq.com>
2016-11-02 22:37:05 +0800

    添加图片滚动加载

---
+++ src/components/Mloading.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="m-loading" v-show="loading">
+    <span class="row1"></span>
+    <span class="row2"></span>
+    <span class="row3"></span>
+    <span class="row4"></span>
+    <span class="row5"></span>
+    <p>加载中...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['loading']
+}
+</script>
+
+<style lang="scss">
+  .m-loading {
+    position: relative;
+    margin: auto;
+    margin-top: 20px;
+    width: 200px;
+    height: 100px;
+
+    span {
+      position: absolute;
+      display: inline-block;
+      width: 15px;
+      height: 15px;
+      border-radius: 15px;
+      background-color: white;
+    }
+
+    p {
+      color: black;
+      padding-top: 20px;
+      margin-left: 25px;
+    }
+
+    span.row1 {
+      left: 0px;
+      background-color: #e14545;
+      animation: row1 1s ease-out infinite;
+    }
+
+    span.row2 {
+      left: 25px;
+      background-color: #fcb421;
+    }
+
+    span.row3 {
+      left: 50px;
+      background-color: #1aadc0;
+    }
+
+    span.row4 {
+      left: 75px;
+      background-color: #aade26;
+    }
+
+    span.row5 {
+      left: 100px;
+      background-color: #5e5b9b;
+      animation: row5 1s ease-out infinite;
+    }
+
+    @keyframes row1 {
+      0% {
+        transform: translate3d(0, 0, 0);
+      }
+
+      50% {
+        transform: translate3d(-10px, -30px, 0);
+      }
+
+      100% {
+        transform: translate3d(0, 0, 0);
+      }
+    }
+
+    @keyframes row5 {
+      0% {
+        transform: translate3d(10px, -30px, 0);
+      }
+
+      50% {
+        transform: translate3d(0, 0, 0);
+      }
+
+      100% {
+        transform: translate3d(10px, -30px, 0);
+      }
+    }
+  }
+</style>
--- src/view/Photo.vue
+++ src/view/Photo.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="wraper">
     <NavBar></NavBar>
-    <div class="content">
+    <div class="content photo-c">
       <Loading :loading="loading"></Loading>
-      <div class="photo-list">
+      <div class="photo-list" ref="photoList">
         <div class="photo-item" v-for="photo in photos">
           <img :src="photo.urls.small" :alt="photo.id"/>
         </div>
       </div>
+      <Mloading :loading="mloading"></Mloading>
     </div>
   </div>
 </template>
@@ -15,31 +16,58 @@
 <script>
 import NavBar from '../components/Nav'
 import Loading from '../components/Loading'
+import Mloading from '../components/Mloading'
 export default {
   data () {
     return {
       loading: true,
+      mloading: false,
       url: 'https://api.unsplash.com/photos?client_id=fc1ad074b94abad2fa784ab7740425e91b4ec8db73473371fa36aaa88e866658&page=',
       photos: [],
-      page: 1
+      page: 1,
+      listHeight: 0,
+      first: true
     }
   },
   computed: {
   },
   methods: {
+    start () {
+      this.$http.get(this.url + this.page).then((response) => {
+        this.first === true ? this.loading = false : this.mloading = false
+        this.first = false
+        response.body.forEach((val) => {
+          this.photos.push(val)
+        })
+        this.$nextTick(() => {
+          this.listHeight = window.getComputedStyle(this.$refs.photoList).height
+        })
+      }, (response) => {
+        this.first === true ? this.loading = false : this.mloading = false
+        console.error('请求失败！')
+      })
+    }
   },
   components: {
     NavBar,
-    Loading
+    Loading,
+    Mloading
   },
   mounted () {
-    this.$http.get(this.url + this.page).then((response) => {
-      this.loading = false
-      this.photos = response.body
-    }, (response) => {
-      this.loading = false
-      console.error('请求失败！')
-    })
+    this.start()
+    // 滚动加载
+    window.onscroll = () => {
+      if (this.mloading === false) {
+        var d = this.listHeight > 0 ? this.listHeight : document.body.height
+        // 记录滚动条高度
+        var t = document.documentElement.scrollTop || document.body.scrollTop
+        if (d - t < 400) {
+          this.mloading = true
+          this.page += 1
+          this.start()
+        }
+      }
+    }
   }
 }
 </script>
@@ -47,10 +75,10 @@ export default {
 <style lang="scss">
   .photo-list {
     margin-top: 30px;
-    width: 1200px;
-    -webkit-column-count: 3;
-    -moz-column-count: 3;
-    column-count: 3;
+    width: 80%;
+    -webkit-column-count: 4;
+    -moz-column-count: 4;
+    column-count: 4;
     -moz-column-gap: 0px; /* Firefox */
     -webkit-column-gap: 0px; /* Safari and Chrome */
     column-gap: 0px;
@@ -58,7 +86,7 @@ export default {
     .photo-item {
       float: left;
       display: inline-block;
-      margin: 5px;
+      margin: 0px  5px;
 
       img {
         width: 100%;
@@ -69,8 +97,9 @@ export default {
 
   @media screen and (max-width: 1400px) {
     .photo-list {
+      margin: auto;
       margin-top: 15px;
-      width: 800px;
+      width: 80%;
       -webkit-column-count: 3;
       -moz-column-count: 3;
       column-count: 3;
