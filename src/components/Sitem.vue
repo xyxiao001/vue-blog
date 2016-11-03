f4043cb2f704cdba08e1f5e049c2855fa5688232
xyxiao <463618622@qq.com>
2016-11-03 20:27:17 +0800

    add story item

---
+++ src/components/Sitem.vue
@@ -0,0 +1,89 @@
+<template>
+  <a class="s-item">
+    <div class="left">
+      <img :src="item.img" alt="item.title" />
+    </div>
+    <div class="right">
+      <span class="title">{{ item.title }}</span>
+      <p>{{ item.desc }}</p>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: ['item']
+}
+</script>
+
+<style lang="scss">
+  .s-item {
+    display: inline-block;
+    width: 500px;
+    height: 105px;
+    margin: 20px;
+    background-color: #fff;
+    border: 1px solid #eee;
+    -webkit-box-shadow: 0 1px 4px rgba(0, 0, 0, 0.27), 0 0 60px rgba(0, 0, 0, 0.06) inset;
+    -moz-box-shadow: 0 1px 4px rgba(0, 0, 0, 0.27), 0 0 40px rgba(0, 0, 0, 0.06) inset;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.27), 0 0 40px rgba(0, 0, 0, 0.06) inset;
+    position: relative;
+    *zoom: 1;
+    overflow: hidden;
+    cursor: pointer;
+
+    .left {
+      float: left;
+      width: 100px;
+
+      img {
+        height: 100px;
+      }
+    }
+
+    .right {
+      float: left;
+      width: 335px;
+      margin-left: 60px;
+
+      .title {
+        font-size: 16px;
+        color: #62bbc3;
+      }
+
+      p {
+        font-size: 13px;
+      }
+    }
+  }
+
+  @media screen and (max-width: 500px) {
+    .s-item {
+      width: 100%;
+      margin: 5px;
+
+      .left {
+        float: left;
+        width: 30%;
+
+        img {
+          width: 100%;
+        }
+      }
+
+      .right {
+        width: 70%;
+        margin: 0px;
+
+        .title {
+          margin-left: 2%;
+        }
+
+        p {
+          margin-left: 2%;
+          width: 95%;
+        }
+      }
+    }
+  }
+</style>
--- src/view/Story.vue
+++ src/view/Story.vue
@@ -2,33 +2,45 @@
   <div class="wraper">
     <NavBar></NavBar>
     <div class="content">
-      123
+      <div class="s-list">
+        <Sitem  v-for="list in lists" :item="list"></Sitem>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import NavBar from '../components/Nav'
+import Sitem from '../components/Sitem'
+
 export default {
   data () {
     return {
+      url: 'https://route.showapi.com/955-1?&showapi_appid=26601&showapi_timestamp=20161103170634&type=dp&showapi_sign=c23526a67ce4ea5bc3fd75123ddf55cc&page=',
+      current: 1,
+      allPages: '',
+      lists: []
     }
   },
   computed: {
   },
   methods: {
     start () {
-      this.$http.get('https://route.showapi.com/955-1?page=1&showapi_appid=26601&showapi_timestamp=20161103170634&type=dp&showapi_sign=c23526a67ce4ea5bc3fd75123ddf55cc').then((response) => {
-        console.log(response.body)
+      this.$http.get(this.url + this.current).then((response) => {
+        const data = response.body.showapi_res_body.pagebean
+        this.allPages = data.allPages
+        this.lists = data.contentlist
       }, (response) => {
         console.error('请求失败！')
       })
     }
   },
   components: {
-    NavBar
+    NavBar,
+    Sitem
   },
   mounted () {
+    this.start()
   }
 }
 </script>
